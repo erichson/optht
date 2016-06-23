678c584e56a4a82a973e0b09f3ed16f7842e8f4e
Benli11 <nbe@st-andrews.ac.uk>
2016-06-22 19:02:16 -0700

    optHT

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
-# optht
 Optimal Hard Threshold for Matrix Denoising
+*******************************************
+
+Off-the-shelf method for determining the optimal singular value truncation
+(hard threshold) for matrix denoising.    
+    
+The method gives the optimal location both in the case of the konwn or unknown noise level.
+
+
+
+Example
+*******
+![example](https://raw.githubusercontent.com/Benli11/data/master/img/optHT2.png)
+![example2](https://raw.githubusercontent.com/Benli11/data/master/img/optHT3.png)
+
+
+Reproduce the example
+*********************
+
+Create some data:
+
+```Python
+import numpy as np
+import scipy as sci
+import matplotlib.pyplot as plt 
+
+t = np.arange(-2,2, 0.01)
+
+Utrue = np.array(( [np.cos(17*t) * np.exp(-t**2) , np.sin(11*t)] )).T
+Strue = np.array(( [2, 0], [0, .5] ))
+Vtrue = np.array(( [np.sin(5*t) * np.exp(-t**2) , np.cos(13*t)] )).T
+
+# construct image
+X = Utrue.dot(Strue).dot(Vtrue.T)
+
+# define the noise level and add
+sigma = 0.5
+X_noisy = X + sigma * np.random.standard_normal(X.shape)
+
+```
+
+Compute the singular value decompositionn (SVD):
+
+```Python
+U,s,Vh = np.linalg.svd(X_noisy, full_matrices=False)
+```
+
+Determine optimal hard threshold and reconstruct image
+```Python
+k = optht(X_noisy, sv=s, sigma=None)
+X_denoised = (U[:, xrange(k)] * s[xrange(k)] ).dot( Vh[xrange(k),:])
+```
+
+Plot the results:
+
+```Python
+
+plt.subplot(131)
+plt.imshow(X, cmap = 'gray', interpolation = 'bicubic')
+plt.title('Original image', fontsize=30)
+plt.axis('off')
+
+plt.subplot(132)
+plt.imshow(X_noisy, cmap = 'gray', interpolation = 'bicubic')
+plt.title('Noisy image, sigma=%s'%sigma, fontsize=30)
+plt.axis('off')
+
+plt.subplot(133)
+plt.imshow(X_denoised, cmap = 'gray', interpolation = 'bicubic')
+rmseSVD = np.sqrt(np.sum( ( X - X_denoised )**2 ) /  np.sum(X**2))
+plt.title('Denoised image,  nrmse=%s '%np.round(rmseSVD,2), fontsize=30)
+plt.axis('off')
+
+```
+
+Plot the singular value spectrum:
+
+```Python
+
+plt.plot( (np.arange(1,s.shape[0]+1)), np.log(s), c='b', marker='o', linestyle='--')
+plt.xlabel('k', fontsize=25)
+plt.ylabel('Log-scaled singular values', fontsize=25)
+plt.tick_params(axis='x', labelsize=25) 
+plt.tick_params(axis='y', labelsize=25) 
+plt.title('Singular value spectrum', fontsize=30)
+plt.axvline(k, c='r', linewidth=2, linestyle='--')
+
+```
+
+
+Notes
+*****
+Code is adapted from Matan Gavish and David Donoho, see [1].
+       
+References
+**********
+[1] Gavish, Matan, and David L. Donoho. 
+"The optimal hard threshold for singular values is 4/sqrt(3)" 
+IEEE Transactions on Information Theory 60.8 (2014): 5040-5053.    
+http://arxiv.org/abs/1305.5870
---
+++ optht.py
@@ -0,0 +1,195 @@
+from __future__ import division
+import numpy as np
+import scipy as sci
+from numpy.testing import assert_raises
+
+
+def optht(beta, sv=None, sigma=None, trace=True):
+    """
+    Optimal hard threshold for singular values
+
+    Off-the-shelf method for determining the optimal singular value truncation
+    (hard threshold) for matrix denoising.    
+    
+    The method gives the optimal location both in the case of the konwn or unknown
+    noise level.
+
+
+    Parameters
+    ----------
+    beta : scalar or array_like
+        Scalar determining the aspect ratio of a matrix, i.e., `beta = m/n', where
+        `m >= n'.
+        Instead the input matrix can be provided and the aspect ratio is determined
+        automatically. 
+        
+    sv : array_like  
+        The singular values for the given input matrix.  
+    
+    sigma : real, optional
+        Noise level if known.
+    
+    trace : bool `{True, False}`
+        Print results.         
+
+    Returns
+    -------
+    k : int
+        Optimal target rank.
+    
+
+    Notes
+    -----
+    Code is adapted from Matan Gavish and David Donoho, see [1].
+       
+    References
+    ----------
+    [1] Gavish, Matan, and David L. Donoho. 
+    "The optimal hard threshold for singular values is 4/sqrt(3)" 
+    IEEE Transactions on Information Theory 60.8 (2014): 5040-5053.    
+    http://arxiv.org/abs/1305.5870
+    
+    Examples
+    --------
+
+
+    
+    % Usage in known noise level:
+    %
+    %   Given an m-by-n matrix Y known to be low rank and observed in white noise 
+    %   with mean zero and known variance sigma^2, form a denoised matrix Xhat by:
+    %
+    %   [U D V] = svd(Y);
+    %   y = diag(Y);
+    %   y( y < (optimal_SVHT_coef(m/n,1) * sqrt(n) * sigma) ) = 0;
+    %   Xhat = U * diag(y) * V';
+    % 
+    %
+    % Usage in unknown noise level:
+    %
+    %   Given an m-by-n matrix Y known to be low rank and observed in white
+    %   noise with mean zero and unknown variance, form a denoised matrix 
+    %   Xhat by:
+    %  
+    %   [U D V] = svd(Y); 
+    %   y = diag(Y); 
+    %   y( y < (optimal_SVHT_coef_sigma_unknown(m/n,0) * median(y)) ) = 0; 
+    %   Xhat = U * diag(y) * V';
+    % 
+    % -----------------------------------------------------------------------------
+
+
+    """
+
+    #*************************************************************************
+    #***        Author: N. Benjamin Erichson <nbe@st-andrews.ac.uk>        ***
+    #***                              <2016>                               ***
+    #***                       License: BSD 3 clause                       ***
+    #*************************************************************************
+          
+    
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # compute aspect ratio of the input matrix
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    if isinstance(beta, (np.ndarray)):
+        m = min(beta.shape)
+        n = max(beta.shape)
+        beta = m/n
+    
+    if beta < 0 or beta > 1:
+        raise ValueError('beta must be in (0,1].')
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Sigma unknown
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            
+    if sigma is None:
+        if trace==True: print('Sigma unknown:')
+        if trace==True: print('*************')
+
+        coef = optimal_SVHT_coef_sigma_unknown(beta)
+        if trace==True: print('approximated coefficent w(beta): ', coef) 
+        coef = optimal_SVHT_coef_sigma_known(beta) / np.sqrt(MedianMarcenkoPastur(beta))
+        if trace==True: print('optimal coefficent w(beta): ', coef) 
+
+
+        if sv is not None:
+            cutoff = coef * np.median(sv)
+            if trace==True: print('cutoff value: ', cutoff)
+
+            k = np.max( np.where( sv>cutoff ) ) + 1
+            if trace==True: print('target rank: ', k)
+
+            return k
+    # End else   
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Sigma known
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                
+    else:
+        if trace==True: print('Sigma known:')
+        if trace==True: print('*************')        
+        
+        coef = optimal_SVHT_coef_sigma_known(beta)
+        if trace==True: print('w(beta) value: ', coef) 
+
+        
+        if sv is not None:
+            cutoff = coef * np.sqrt(len(sv)) * sigma
+            if trace==True: print('cutoff value: ', cutoff)
+            
+            k = np.max( np.where( sv>cutoff ) ) + 1
+            if trace==True: print('target rank: ', k)
+            
+            return k 
+    # End else        
+    return coef
+        
+
+# Equation (11)
+def optimal_SVHT_coef_sigma_known(beta):
+    return np.sqrt(2 * (beta + 1) + (8 * beta) / (beta + 1 + np.sqrt(beta**2 + 14 * beta +1)))
+
+# Equation (5)
+def optimal_SVHT_coef_sigma_unknown(beta):
+    return 0.56*beta**3 - 0.95*beta**2 + 1.82*beta + 1.43
+
+
+# Marcenko-Pastur distribution
+def MarPas(x, topSpec, botSpec, beta):
+    if (topSpec-x)*(x-botSpec) > 0:
+        return np.sqrt((topSpec-x) * (x-botSpec)) / (beta * x) / (2 * np.pi)
+    else:
+        return 0
+
+
+def MedianMarcenkoPastur(beta):
+    botSpec = lobnd = (1 - np.sqrt(beta))**2
+    topSpec = hibnd = (1 + np.sqrt(beta))**2  
+    change = 1
+
+    while(change & ((hibnd-lobnd) > .001 ) ):
+        change = 0
+        x = np.linspace(lobnd, hibnd, 10)
+        y = np.zeros_like(x)
+        for i in xrange(len(x)):
+            yi, err = sci.integrate.quad(MarPas, a=x[i], b=topSpec, args=(topSpec, botSpec, beta))
+            y[i] = 1-yi
+
+        if np.any( y < 0.5 ):
+            lobnd = np.max( x[y < 0.5] )
+            change = 1
+            
+        if np.any( y > 0.5 ):
+            hibnd = np.min( x[y > 0.5] )
+            change = 1
+            
+    return (hibnd+lobnd) / 2.
+
+
+
+
+
+
+
