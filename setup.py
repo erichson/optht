9f2e544f737c3fa357f32363a94c2b31abc04c40
Ben Erichson <12964143+erichson@users.noreply.github.com>
2021-07-21 13:58:55 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,19 @@ setuptools.setup(
     version='0.2.0',
     description='Optimal hard threshold for matrix denoising',
     long_description=readme,
+    long_description_content_type="text/markdown",
     author='N. Benjamin Erichson, Steven Dahdah',
     author_email='erichson@berkeley.edu, Steven.Dahdah@mail.mcgill.ca',
     url='https://github.com/erichson/optht',
+    project_urls={
+        "Bug Tracker": "https://github.com/erichson/optht/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     packages=setuptools.find_packages(exclude=('tests', 'examples')),
     install_requires=['numpy', 'scipy'],
+    python_requires=">=3.6",
 )
